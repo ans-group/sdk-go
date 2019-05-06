693974ce631e98eaddc42e637886d02c294d8bda
Lee Spottiswood <lee.spottiswood@ukfast.co.uk>
2019-05-06 15:32:14 +0100

    add credit retrieval for account service and ecloud service

---
+++ pkg/service/ecloud/service_credit_test.go
@@ -0,0 +1,57 @@
+package ecloud
+
+import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"github.com/ukfast/sdk-go/pkg/connection"
+	"github.com/ukfast/sdk-go/test/mocks"
+)
+
+func TestGetCredits(t *testing.T) {
+	t.Run("Valid", func(t *testing.T) {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		c := mocks.NewMockConnection(mockCtrl)
+
+		s := Service{
+			connection: c,
+		}
+
+		c.EXPECT().Get("/ecloud/v1/credits", gomock.Any()).Return(&connection.APIResponse{
+			Response: &http.Response{
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte("{\"data\":[{\"type\":\"test credit\"}]}"))),
+				StatusCode: 200,
+			},
+		}, nil).Times(1)
+
+		credits, err := s.GetCredits(connection.APIRequestParameters{})
+
+		assert.Nil(t, err)
+		assert.Equal(t, "test credit", credits[0].Type)
+	})
+
+	t.Run("ConnectionError_ReturnsError", func(t *testing.T) {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		c := mocks.NewMockConnection(mockCtrl)
+
+		s := Service{
+			connection: c,
+		}
+
+		c.EXPECT().Get("/ecloud/v1/credits", gomock.Any()).Return(&connection.APIResponse{}, errors.New("test error 1")).Times(1)
+
+		_, err := s.GetCredits(connection.APIRequestParameters{})
+
+		assert.NotNil(t, err)
+		assert.Equal(t, "test error 1", err.Error())
+	})
+}
