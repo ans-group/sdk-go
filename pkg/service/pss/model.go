50e1ef2eea4e13ba5fa4a29b72b3e0e7fb27d4e7
Lee Spottiswood <me@lee.io>
2024-07-11 16:32:19 +0100

    add PSS v2 models

--- pkg/service/pss/model.go
+++ pkg/service/pss/model.go
@@ -116,39 +116,425 @@ type CaseOption struct {
 type CaseType string
 
 const (
-	CaseTypeAllow CaseType = "allow"
-	CaseTypeDeny  CaseType = "deny"
+	CaseTypeChange   CaseType = "Change"
+	CaseTypeIncident CaseType = "Incident"
+	CaseTypeProblem  CaseType = "Problem"
 )
 
 var CaseTypeEnum connection.Enum[CaseType] = []CaseType{
-	CaseTypeAllow,
-	CaseTypeDeny,
+	CaseTypeChange,
+	CaseTypeIncident,
+	CaseTypeProblem,
 }
 
-func test() {
-	_, _ = CaseTypeEnum.Parse("allow")
+type CaseStatus string
 
+const (
+	CaseStatusInProgress                   CaseStatus = "In Progress"
+	CaseStatusOnHold                       CaseStatus = "On Hold"
+	CaseStatusOpen                         CaseStatus = "Open"
+	CaseStatusCaseCreated                  CaseStatus = "Case Created"
+	CaseStatusResolved                     CaseStatus = "Resolved"
+	CaseStatusPendingAssessment            CaseStatus = "Pending Assessment"
+	CaseStatusPendingSME                   CaseStatus = "Pending SME"
+	CaseStatusProblemAssessment            CaseStatus = "Problem Assessment"
+	CaseStatusClosed                       CaseStatus = "Closed"
+	CaseStatusPendingCustomersApproval     CaseStatus = "Pending Customers Approval"
+	CaseStatusPendingANSCAB                CaseStatus = "Pending ANS CAB"
+	CaseStatusPendingImplementation        CaseStatus = "Pending Implementation"
+	CaseStatusImplementationStarted        CaseStatus = "Implementation Started"
+	CaseStatusDelivered                    CaseStatus = "Delivered"
+	CaseStatusRejected                     CaseStatus = "Rejected"
+	CaseStatusScheduleChange               CaseStatus = "Schedule Change"
+	CaseStatusCustomerClosure              CaseStatus = "Customer Closure"
+	CaseStatusProblemSolved                CaseStatus = "Problem Solved"
+	CaseStatusInformationProvided          CaseStatus = "Information Provided"
+	CaseStatusClosedWithCustomerConsent    CaseStatus = "Closed with Customer Consent"
+	CaseStatusClosedCustomerNotContactable CaseStatus = "Closed - Customer Not Contactable"
+	CaseStatusClosedWithTeamLeaderApproval CaseStatus = "Closed with Team Leader Approval"
+	CaseStatusCaseClosed                   CaseStatus = "Case Closed"
+	CaseStatusANSOperatorError             CaseStatus = "ANS Operator Error"
+	CaseStatusCustomerError                CaseStatus = "Customer Error"
+	CaseStatusOutofScopeofServiceContract  CaseStatus = "Out of Scope of Service Contract"
+)
+
+type ChangeCaseType string
+
+const (
+	ChangeCaseTypeNormal             ChangeCaseType = "Normal"
+	ChangeCaseTypeStandard           ChangeCaseType = "Standard"
+	ChangeCaseTypeEmergency          ChangeCaseType = "Emergency"
+	ChangeCaseTypeProject            ChangeCaseType = "Project"
+	ChangeCaseTypeCentreOfExcellence ChangeCaseType = "Centre of Excellence"
+)
+
+var ChangeCaseTypeEnum connection.Enum[ChangeCaseType] = []ChangeCaseType{
+	ChangeCaseTypeNormal,
+	ChangeCaseTypeStandard,
+	ChangeCaseTypeEmergency,
+	ChangeCaseTypeProject,
+	ChangeCaseTypeCentreOfExcellence,
 }
 
-// Case represents a PSS case
-type Case struct {
-	ID                    string   `json:"id"`
-	CaseType              string   `json:"case_type"`
-	Title                 string   `json:"title"`
-	Description           string   `json:"description"`
-	IsSecurity            bool     `json:"is_security"`
-	Type                  CaseType `json:"type"`
-	Priority              string   `json:"priority"`
-	Squad                 string   `json:"squad"`
-	Status                string   `json:"status"`
-	CustomerReference     string   `json:"customer_reference"`
-	Source                string   `json:"source"`
-	Contact               string   `json:"contact"`
-	CriteriaForResolution string   `json:"criteria_for_resolution"`
-	ServiceName           string   `json:"service_name"`
-	Owner                 string   `json:"owner"`
-	ResponseTarget        string   `json:"response_target"`
-	ResolutionTarget      string   `json:"resolution_target"`
-	CreatedAt             string   `json:"created_at"`
-	UpdatedAt             string   `json:"updated_at"`
+type ChangeCasePriority string
+
+const (
+	ChangeCasePriorityCR1 ChangeCasePriority = "CR1"
+	ChangeCasePriorityCR2 ChangeCasePriority = "CR2"
+	ChangeCasePriorityCR3 ChangeCasePriority = "CR3"
+	ChangeCasePriorityCR4 ChangeCasePriority = "CR4"
+	ChangeCasePriorityCR5 ChangeCasePriority = "CR5"
+	ChangeCasePriorityCRS ChangeCasePriority = "CR-S"
+	ChangeCasePriorityCRE ChangeCasePriority = "CR-E"
+	ChangeCasePriorityCRP ChangeCasePriority = "CR-P"
+)
+
+var ChangeCasePriorityEnum connection.Enum[ChangeCasePriority] = []ChangeCasePriority{
+	ChangeCasePriorityCR1,
+	ChangeCasePriorityCR2,
+	ChangeCasePriorityCR3,
+	ChangeCasePriorityCR4,
+	ChangeCasePriorityCR5,
+	ChangeCasePriorityCRS,
+	ChangeCasePriorityCRE,
+	ChangeCasePriorityCRP,
+}
+
+var CaseStatusEnum connection.Enum[CaseStatus] = []CaseStatus{
+	CaseStatusInProgress,
+	CaseStatusOnHold,
+	CaseStatusOpen,
+	CaseStatusCaseCreated,
+	CaseStatusResolved,
+	CaseStatusPendingAssessment,
+	CaseStatusPendingSME,
+	CaseStatusProblemAssessment,
+	CaseStatusClosed,
+	CaseStatusPendingCustomersApproval,
+	CaseStatusPendingANSCAB,
+	CaseStatusPendingImplementation,
+	CaseStatusImplementationStarted,
+	CaseStatusDelivered,
+	CaseStatusRejected,
+	CaseStatusScheduleChange,
+	CaseStatusCustomerClosure,
+	CaseStatusProblemSolved,
+	CaseStatusInformationProvided,
+	CaseStatusClosedWithCustomerConsent,
+	CaseStatusClosedCustomerNotContactable,
+	CaseStatusClosedWithTeamLeaderApproval,
+	CaseStatusCaseClosed,
+	CaseStatusANSOperatorError,
+	CaseStatusCustomerError,
+	CaseStatusOutofScopeofServiceContract,
+}
+
+type ChangeCaseStage string
+
+const (
+	ChangeCaseStageCaseType              ChangeCaseStage = "Case Type"
+	ChangeCaseStageIdentify              ChangeCaseStage = "Identify"
+	ChangeCaseStagePendingAssessment     ChangeCaseStage = "Pending Assessment"
+	ChangeCaseStageApprovals             ChangeCaseStage = "Approvals"
+	ChangeCaseStagePendingImplementation ChangeCaseStage = "Pending Implementation"
+	ChangeCaseStageImplementationStarted ChangeCaseStage = "Implementation Started"
+	ChangeCaseStageDelivered             ChangeCaseStage = "Delivered"
+)
+
+var ChangeCaseStageEnum connection.Enum[ChangeCaseStage] = []ChangeCaseStage{
+	ChangeCaseStageCaseType,
+	ChangeCaseStageIdentify,
+	ChangeCaseStagePendingAssessment,
+	ChangeCaseStageApprovals,
+	ChangeCaseStagePendingImplementation,
+	ChangeCaseStageImplementationStarted,
+	ChangeCaseStageDelivered,
+}
+
+type ChangeCaseImpact string
+
+const (
+	ChangeCaseImpactLow    ChangeCaseImpact = "Low"
+	ChangeCaseImpactMedium ChangeCaseImpact = "Medium"
+	ChangeCaseImpactHigh   ChangeCaseImpact = "High"
+)
+
+var ChangeCaseImpactEnum connection.Enum[ChangeCaseImpact] = []ChangeCaseImpact{
+	ChangeCaseImpactLow,
+	ChangeCaseImpactMedium,
+	ChangeCaseImpactHigh,
+}
+
+type ChangeCaseRisk string
+
+const (
+	ChangeCaseRiskLow    ChangeCaseRisk = "Low"
+	ChangeCaseRiskMedium ChangeCaseRisk = "Medium"
+	ChangeCaseRiskHigh   ChangeCaseRisk = "High"
+)
+
+var ChangeCaseRiskEnum connection.Enum[ChangeCaseRisk] = []ChangeCaseRisk{
+	ChangeCaseRiskLow,
+	ChangeCaseRiskMedium,
+	ChangeCaseRiskHigh,
+}
+
+type IncidentCaseType string
+
+const (
+	IncidentCaseTypeFault                IncidentCaseType = "Fault"
+	IncidentCaseTypeServiceRequest       IncidentCaseType = "Service Request"
+	IncidentCaseTypeChangeAdvisory       IncidentCaseType = "Change Advisory"
+	IncidentCaseTypeProjectIncident      IncidentCaseType = "Project Incident"
+	IncidentCaseTypeAccessRequest        IncidentCaseType = "Access Request"
+	IncidentCaseTypeScheduledMaintenance IncidentCaseType = "Scheduled Maintenance"
+	IncidentCaseTypeArchitecturalAdvice  IncidentCaseType = "Architectural Advice"
+	IncidentCaseTypeSecurityEvent        IncidentCaseType = "Security Event"
+)
+
+var IncidentCaseTypeEnum connection.Enum[IncidentCaseType] = []IncidentCaseType{
+	IncidentCaseTypeFault,
+	IncidentCaseTypeServiceRequest,
+	IncidentCaseTypeChangeAdvisory,
+	IncidentCaseTypeProjectIncident,
+	IncidentCaseTypeAccessRequest,
+	IncidentCaseTypeScheduledMaintenance,
+	IncidentCaseTypeArchitecturalAdvice,
+	IncidentCaseTypeSecurityEvent,
+}
+
+type IncidentCasePriority string
+
+const (
+	IncidentCasePriorityP1 IncidentCasePriority = "P1"
+	IncidentCasePriorityP2 IncidentCasePriority = "P2"
+	IncidentCasePriorityP3 IncidentCasePriority = "P3"
+	IncidentCasePriorityP4 IncidentCasePriority = "P4"
+	IncidentCasePriorityP5 IncidentCasePriority = "P5"
+)
+
+var IncidentCasePriorityEnum connection.Enum[IncidentCasePriority] = []IncidentCasePriority{
+	IncidentCasePriorityP1,
+	IncidentCasePriorityP2,
+	IncidentCasePriorityP3,
+	IncidentCasePriorityP4,
+	IncidentCasePriorityP5,
+}
+
+type IncidentCaseImpact string
+
+const (
+	IncidentCaseImpactMajor    IncidentCaseImpact = "Major"
+	IncidentCaseImpactModerate IncidentCaseImpact = "Moderate"
+	IncidentCaseImpactMinor    IncidentCaseImpact = "Minor"
+)
+
+var IncidentCaseImpactEnum connection.Enum[IncidentCaseImpact] = []IncidentCaseImpact{
+	IncidentCaseImpactMajor,
+	IncidentCaseImpactModerate,
+	IncidentCaseImpactMinor,
+}
+
+type ProblemCaseType string
+
+const (
+	ProblemCaseTypeRCA              ProblemCaseType = "RCA"
+	ProblemCaseTypeKnownError       ProblemCaseType = "Known Error"
+	ProblemCaseTypeVulnerability    ProblemCaseType = "Vulnerability"
+	ProblemCaseTypeIssueReOccurence ProblemCaseType = "Issue Re-Occurence"
+	ProblemCaseTypeNonCompliance    ProblemCaseType = "Non-Compliance"
+	ProblemCaseTypeBugFix           ProblemCaseType = "Bug Fix"
+)
+
+var ProblemCaseTypeEnum connection.Enum[ProblemCaseType] = []ProblemCaseType{
+	ProblemCaseTypeRCA,
+	ProblemCaseTypeKnownError,
+	ProblemCaseTypeVulnerability,
+	ProblemCaseTypeIssueReOccurence,
+	ProblemCaseTypeNonCompliance,
+	ProblemCaseTypeBugFix,
+}
+
+type ProblemCasePriority string
+
+const (
+	ProblemCasePriorityPRB1   ProblemCasePriority = "PRB1"
+	ProblemCasePriorityPRB2   ProblemCasePriority = "PRB2"
+	ProblemCasePriorityPRB3   ProblemCasePriority = "PRB3"
+	ProblemCasePriorityPRB4   ProblemCasePriority = "PRB4"
+	ProblemCasePriorityPRB5   ProblemCasePriority = "PRB5"
+	ProblemCasePriorityPRBRCA ProblemCasePriority = "PRB-RCA"
+	ProblemCasePriorityPRBMI  ProblemCasePriority = "PRB-MI"
+)
+
+var ProblemCasePriorityEnum connection.Enum[ProblemCasePriority] = []ProblemCasePriority{
+	ProblemCasePriorityPRB1,
+	ProblemCasePriorityPRB2,
+	ProblemCasePriorityPRB3,
+	ProblemCasePriorityPRB4,
+	ProblemCasePriorityPRB5,
+	ProblemCasePriorityPRBRCA,
+	ProblemCasePriorityPRBMI,
+}
+
+type ProblemCaseUrgency string
+
+const (
+	ProblemCaseUrgencySystemServiceDown ProblemCaseUrgency = "System / Service Down"
+	ProblemCaseUrgencySystemAffected    ProblemCaseUrgency = "System / Service Affected"
+	ProblemCaseUrgencyUserAffected      ProblemCaseUrgency = "User Down / Affected"
+)
+
+var ProblemCaseUrgencyEnum connection.Enum[ProblemCaseUrgency] = []ProblemCaseUrgency{
+	ProblemCaseUrgencySystemServiceDown,
+	ProblemCaseUrgencySystemAffected,
+	ProblemCaseUrgencyUserAffected,
+}
+
+type ProblemCaseDetailedImpact string
+
+const (
+	ProblemCaseDetailedImpactMajor    ProblemCaseDetailedImpact = "Major"
+	ProblemCaseDetailedImpactModerate ProblemCaseDetailedImpact = "Moderate"
+	ProblemCaseDetailedImpactMinor    ProblemCaseDetailedImpact = "Minor"
+)
+
+var ProblemCaseDetailedImpactEnum connection.Enum[ProblemCaseDetailedImpact] = []ProblemCaseDetailedImpact{
+	ProblemCaseDetailedImpactMajor,
+	ProblemCaseDetailedImpactModerate,
+	ProblemCaseDetailedImpactMinor,
+}
+
+type ProblemCaseKnownWorkaround string
+
+const (
+	ProblemCaseKnownWorkaroundNotCurrentlyKnown                  ProblemCaseKnownWorkaround = "Not Currently Known"
+	ProblemCaseKnownWorkaroundTemporaryWorkaround                ProblemCaseKnownWorkaround = "Temporary Workaround"
+	ProblemCaseKnownWorkaroundPermanentWorkaround                ProblemCaseKnownWorkaround = "Permanent Workaround"
+	ProblemCaseKnownWorkaroundCurrentlyUnavailableAwaitingVendor ProblemCaseKnownWorkaround = "Currently Unavailable / Awaiting Vendor"
+)
+
+var ProblemCaseKnownWorkaroundEnum connection.Enum[ProblemCaseKnownWorkaround] = []ProblemCaseKnownWorkaround{
+	ProblemCaseKnownWorkaroundNotCurrentlyKnown,
+	ProblemCaseKnownWorkaroundTemporaryWorkaround,
+	ProblemCaseKnownWorkaroundPermanentWorkaround,
+	ProblemCaseKnownWorkaroundCurrentlyUnavailableAwaitingVendor,
+}
+
+type ProblemCaseKnownCause string
+
+const (
+	ProblemCaseKnownCauseNotCurrentlyKnown     ProblemCaseKnownCause = "Not Currently Known"
+	ProblemCaseKnownCauseSoftwareDefect        ProblemCaseKnownCause = "Software Defect"
+	ProblemCaseKnownCauseHardwareFailure       ProblemCaseKnownCause = "Hardware Failure"
+	ProblemCaseKnownCauseCommunicationsFailure ProblemCaseKnownCause = "Communications Failure"
+	ProblemCaseKnownCauseHardwareLimitation    ProblemCaseKnownCause = "Hardware Limitation"
+	ProblemCaseKnownCauseBusinessProcessIssue  ProblemCaseKnownCause = "Business Process Issue"
+	ProblemCaseKnownCauseUser                  ProblemCaseKnownCause = "User"
+)
+
+var ProblemCaseKnownCauseEnum connection.Enum[ProblemCaseKnownCause] = []ProblemCaseKnownCause{
+	ProblemCaseKnownCauseNotCurrentlyKnown,
+	ProblemCaseKnownCauseSoftwareDefect,
+	ProblemCaseKnownCauseHardwareFailure,
+	ProblemCaseKnownCauseCommunicationsFailure,
+	ProblemCaseKnownCauseHardwareLimitation,
+	ProblemCaseKnownCauseBusinessProcessIssue,
+	ProblemCaseKnownCauseUser,
+}
+
+// IncidentCase represents a PSS incident-type case
+type IncidentCase struct {
+	ID                    string               `json:"id"`
+	CaseType              CaseType             `json:"case_type"`
+	Title                 string               `json:"title"`
+	Description           string               `json:"description"`
+	IsSecurity            bool                 `json:"is_security"`
+	Type                  IncidentCaseType     `json:"type"`
+	Priority              IncidentCasePriority `json:"priority"`
+	Squad                 string               `json:"squad"`
+	Status                CaseStatus           `json:"status"`
+	CustomerReference     string               `json:"customer_reference"`
+	Source                string               `json:"source"`
+	ContactID             int                  `json:"contact_id"`
+	Contact               string               `json:"contact"`
+	CriteriaForResolution string               `json:"criteria_for_resolution"`
+	ServiceName           string               `json:"service_name"`
+	Owner                 string               `json:"owner"`
+	ResponseTarget        string               `json:"response_target"`
+	ResolutionTarget      string               `json:"resolution_target"`
+	CreatedAt             string               `json:"created_at"`
+	UpdatedAt             string               `json:"updated_at"`
+	CategoryID            string               `json:"category_id"`
+	SupportedServiceID    string               `json:"supported_service_id"`
+	Impact                IncidentCaseImpact   `json:"impact"`
+}
+
+// IncidentCase represents a PSS change-type case
+type ChangeCase struct {
+	ID                         string             `json:"id"`
+	CaseType                   CaseType           `json:"case_type"`
+	Title                      string             `json:"title"`
+	Description                string             `json:"description"`
+	IsSecurity                 bool               `json:"is_security"`
+	Type                       ChangeCaseType     `json:"type"`
+	Priority                   ChangeCasePriority `json:"priority"`
+	Squad                      string             `json:"squad"`
+	Status                     CaseStatus         `json:"status"`
+	Contact                    string             `json:"contact"`
+	ScheduledDate              string             `json:"scheduled_date"`
+	CustomerReference          string             `json:"customer_reference"`
+	Owner                      string             `json:"owner"`
+	Stage                      ChangeCaseStage    `json:"stage"`
+	Reason                     string             `json:"reason"`
+	Impact                     ChangeCaseImpact   `json:"impact"`
+	Risk                       ChangeCaseRisk     `json:"risk"`
+	ServiceName                string             `json:"service_name"`
+	EstimatedTimeForCompletion int                `json:"estimated_time_for_completion"`
+	EstimatedDowntime          int                `json:"estimated_downtime"`
+	ImpactAndPotentialRisks    string             `json:"impact_and_potential_risks"`
+	BackoutPlan                string             `json:"backout_plan"`
+	ImplementationSteps        string             `json:"implementation_steps"`
+	TestSteps                  string             `json:"test_steps"`
+	CreatedAt                  string             `json:"created_at"`
+	UpdatedAt                  string             `json:"updated_at"`
+	CategoryID                 string             `json:"category_id"`
+	SupportedServiceID         string             `json:"supported_service_id"`
+}
+
+// IncidentCase represents a PSS problem-type case
+type ProblemCase struct {
+	ID                     string                     `json:"id"`
+	CaseType               CaseType                   `json:"case_type"`
+	Title                  string                     `json:"title"`
+	Description            string                     `json:"description"`
+	IsSecurity             bool                       `json:"is_security"`
+	Type                   ProblemCaseType            `json:"type"`
+	Priority               ProblemCasePriority        `json:"priority"`
+	Squad                  string                     `json:"squad"`
+	Status                 CaseStatus                 `json:"status"`
+	CurrentAssignee        string                     `json:"current_assignee"`
+	Urgency                ProblemCaseUrgency         `json:"urgency"`
+	DetailedImpact         ProblemCaseDetailedImpact  `json:"detailed_impact"`
+	KnownWorkaround        ProblemCaseKnownWorkaround `json:"known_workaround"`
+	KnownWorkaroundDetails string                     `json:"known_workaround_details"`
+	KnownCause             ProblemCaseKnownCause      `json:"known_cause"`
+	CreatedAt              string                     `json:"created_at"`
+	UpdatedAt              string                     `json:"updated_at"`
+}
+
+// CaseUpdate represents a PSS case update
+type CaseUpdate struct {
+	ID          string `json:"id"`
+	Subject     string `json:"subject"`
+	Description string `json:"description"`
+	Contact     string `json:"contact"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+}
+
+// CaseUpdate represents a PSS case update
+type SupportedService struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
 }
